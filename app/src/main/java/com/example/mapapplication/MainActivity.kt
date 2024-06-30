34d0b587360732e42948207b745e7cf43ababd2d
chankaran2996 <chandrasekaran2996@gmail.com>
2024-06-30 16:42:19 +0530

    first

---
+++ app/src/main/java/com/example/mapapplication/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.mapapplication
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import com.example.mapapplication.databinding.ActivityMainBinding
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+private var mGoogleMap:GoogleMap? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        var binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.mapFragment) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+    override fun onMapReady(googleMap: GoogleMap){
+        mGoogleMap = googleMap
+    }
+
+
+}
+
+
